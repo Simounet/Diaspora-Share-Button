a1e43e32befa19a56279336c88b7529f238e4a85
Simon Alberny <simon.alberny@openwide.fr>
2012-02-17 14:55:02 +0100

    starting jqueryless code

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 /*
 Description: This is a project for a Diaspora's sharing button working with every pod in the world.
-Author: Simounet
-Version: 0.1 
+Author: Simon Alberny
+Version: 0.1
 Author URI: http://www.simounet.net/
 License: GPL2
+
+TODO: add document.write() to place the button at a specific location
 */
 
 /*
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -30,34 +32,69 @@ License: GPL2
 	<title>Diaspora Share Button</title>
 	<script type='text/javascript' src='http://code.jquery.com/jquery-1.7.1.min.js'></script>
 	<script type='text/javascript'>
-		$(document).ready(function() {
-			$("#target").click(function() {
-				if ($("#podname").length == 0) {
-					$("#input").append("<label for='podname'>Pod Name:</label> http://<input type='text' name='podname' id='podname'>");
-				}
-				
-				$("#podname").keyup( function() {
-					if ($("#submit").length == 0) {
-						$("#input").append("<p><input id ='submit' type='submit' value='Submit' name='submit'></p>");
+		// TODO: check compatibility with IE
+		document.onreadystatechange = function () {
+			// TODO: check compatibility with IE
+			if (document.readyState == "complete") {
+				var widget = document.createElement( 'div' );
+				document.body.appendChild( widget );
+				var target = document.createElement( 'a' );
+				target.setAttribute( 'style', 'display:block' );
+				target.setAttribute( 'title', 'Share this at Diaspora!' );
+
+				var img = document.createElement( 'img' );
+				img.setAttribute( 'src', './images/diaspora-share-button.png' );
+				img.setAttribute( 'alt', 'Share this at Diaspora!' );
+				target.appendChild( img );
+				widget.appendChild( target );
+
+				var form = document.createElement( 'form' );
+				form.setAttribute( 'method', 'get' );
+				widget.appendChild( form );
+
+				target.onclick = function() {
+					var podname = target.getElementsByTagName('input');
+					if ( podname.length == 0 ) {
+						var label = document.createElement( 'label' );
+						label.setAttribute( 'for', 'podname' );
+						label.innerHTML = 'Pod Name: http://';
+						form.appendChild( label );
+						var podname = document.createElement( 'input' );
+						podname.setAttribute( 'type', 'text' );
+						podname.setAttribute( 'name', 'podname' );
+						label.appendChild( podname );
+						podname.onkeyup = function() {
+							alert( 'coco' );
+						};
 					}
-				});
-				
-				$('#form').submit(function() {
-					var pod = $("#podname").val();
-				    $("#input").append("<iframe name='iframe' frameborder='0' src='https://" + pod + "/bookmarklet?url=" + encodeURIComponent(window.location.href) + "&amp;title=" + encodeURIComponent(document.title) + "&amp;notes=" + encodeURIComponent('' + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text)) + "&amp;v=1&amp;' allowTransparency='true' width='700' height='490'></iframe>");
-				    return false;
-				});
-			});
-		});
+				}
+
+			}
+		}
+
+		/* onkeyup
+
+							if (document.getElementById('submit') == null && document.getElementById('podname').value != '') {
+								var e = document.createElement('p');
+								e.innerHTML = '<input id="submit" type="submit" value="Submit" name="submit">';
+
+							}
+		*/
+
+
+				/*
+					$('#form').submit(function() {
+						var pod = $("#podname").val();
+						$("#input").append("<iframe name='iframe' frameborder='0' src='https://" + pod + "/bookmarklet?url=" + encodeURIComponent(window.location.href) + "&amp;title=" + encodeURIComponent(document.title) + "&amp;notes=" + encodeURIComponent('' + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text)) + "&amp;v=1&amp;' allowTransparency='true' width='700' height='490'></iframe>");
+						return false;
+					});
+				};
+			}
+		}
+		*/
 	</script>
 </head>
 
 <body>
-	<div id="diaspora-button-box">
-		<div id="target" title="Share this at Diaspora!"><img alt="img_button" src="./images/diaspora-share-button.png" /></div>
-		<form action="#" method="get" id="form">
-			<div id="input"></div>
-		</form>
-	</div>
 </body>
 </html>
